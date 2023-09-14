b5f91f72753cc4a123220bafe2c0b367340d77ab
Guilherme Henrique <ghn.cnc@gmail.com>
2023-09-14 09:52:48 -0300

    iniciando com os tipos primitivos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,20 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Pesquisar mais sobre tipos primitivos//
+        // Pesquisar sobre String que representa texto na aplicação Java//
+
+        String nome = "Guilherme";
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; // Aqui estamos convertendo um número (int) para (short)// 
+
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+        final double VALOR_DE_PI = 3.14; // final representa que o valor da variável é constante//
+
     }
 }
