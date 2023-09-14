70afd02e38b5a8c1cd986d8b799ccf1352e22d4a
Guilherme Henrique <ghn.cnc@gmail.com>
2023-09-14 10:51:54 -0300

    Aprendendo a mexer com operadores.

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,37 @@
+public class Operadores {
+    public static void main(String [] args){
+       // String nomeCompleto = "LINGUAGEM" + "JAVA";//
+       // System.out.println(nomeCompleto);//
+
+      // String concatenacao = "?";
+
+     //  concatenacao = 1+1+1+"1";
+     //  System.out.println(concatenacao);
+     //  concatenacao = 1+"1"+1+1;
+     //  System.out.println(concatenacao);
+     //  concatenacao = 1+"1"+1+"1";
+     //  System.out.println(concatenacao);
+     //  concatenacao = "1"+1+1+1;
+     //  System.out.println(concatenacao);
+     //  concatenacao = "1"+(1+1+1);
+     //  System.out.println(concatenacao);
+
+     //int numero = 5;
+     //numero = - numero; // com o sinal de menos antes da variável número tornamos a expressão negativa//
+     //System.out.println(numero);
+     //numero = numero * -1; // Nessa expressão utilizamos o sinal de multiplicação vezes -1 para convertelo em positivo//
+     //System.out.println(numero);
+
+     //int numero = 5;
+     //x repetição//
+     // numero = numero +1;
+     //System.out.println( ++ numero);
+     //System.out.println(numero);
+
+     boolean variavel = true;
+     System.out.println(!variavel); // transformando verdadeiro em falso (Porém nossa variável continua verdadeira)//
+     variavel = !variavel; // transformando a variável de verdadeiro para falso em definitivo.//
+     System.out.println(variavel);
+    }
+    
+}
